6280499cc1cbd07d83b377ff1e010ac12ab65d26
Andrew Sheridan <sheridan@berkeley.edu>
2019-01-22 22:12:06 -0500

    added minimum functionality for image loading. downloads one file (hardcoded) and prints destination dir contents

---
+++ image_loading.py
@@ -0,0 +1,18 @@
+import os
+import glob
+
+import boto3
+
+s3_bucket_identifier = 'chainsaw-dogs-and-cats'
+
+s3 = boto3.resource('s3', region_name='us-east-1')
+bucket = s3.Bucket(s3_bucket_identifier)
+
+origin_filename = '0.jpg'
+destination_filename = origin_filename
+destination_dir = 'images/' # make sure this has the slash at the end
+
+os.makedirs(os.path.dirname(destination_dir), exist_ok=True)
+bucket.download_file(origin_filename, destination_dir + destination_filename)
+
+print(glob.glob('*/images/*'))
