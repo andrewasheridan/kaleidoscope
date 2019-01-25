665c5083f400d00bcb46a04a6ea7e084e9c512bf
Andrew Sheridan <sheridan@berkeley.edu>
2019-01-25 16:48:25 -0500

    created constants.py to hold constants

---
+++ src/constants.py
@@ -0,0 +1,19 @@
+# TODO: Confirm each filetype is supported in container
+# docs.opencv.org/3.0-beta/modules/imgcodecs/doc/reading_and_writing_images.html#imread
+OPENCV_FILETYPES = [
+    ".bmp",
+    ".dib",
+    ".jpeg",
+    ".jpg",
+    ".jpe",
+    ".jp2",
+    ".png",
+    ".webp",
+    ".pbm",
+    ".pgm",
+    ".ppm",
+    ".sr",
+    ".ras",
+    ".tiff",
+    ".tif",
+]
