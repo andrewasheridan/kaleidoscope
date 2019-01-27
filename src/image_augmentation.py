af6245c7ffe53e671a13400043a915acc3b07118
Andrew Sheridan <sheridan@berkeley.edu>
2019-01-26 22:41:08 -0500

    added type checks to ImageAugmenter

--- src/image_augmentation.py
+++ src/image_augmentation.py
@@ -1,3 +1,30 @@
+from numpy import ndarray
+
 
 class ImageAugmenter(object):
-    pass
+
+    def __init__(self, image, image_name, transformations):
+
+        if type(image) is ndarray:
+            self.image = image
+        else:
+            raise TypeError(
+                "`image` must be numpy array not {}".format(type(image))
+            )
+
+        if type(image_name) is str:
+            self.image_name = image_name
+        else:
+            raise TypeError(
+                "`image_name` must be str not {}".format(type(image_name))
+            )
+
+        if type(transformations) is list:
+            self.transformations = transformations
+        else:
+            raise TypeError(
+                "`transformations` must be list not {}".format(type(transformations))
+            )
+
+
+
