616c2ec08c786c479d569ee7a6bc02974840285c
Andrew Sheridan <sheridan@berkeley.edu>
2019-01-27 19:57:55 -0500

    altered some logic in transformations

--- src/transformations.py
+++ src/transformations.py
@@ -4,10 +4,13 @@ import numpy as np
 
 
 def random_square_crop_with_resize(
-    image, new_side_len=400, interpolation=cv2.INTER_LANCZOS4
+    image, image_name, new_side_len=400, interpolation=cv2.INTER_LANCZOS4
 ):
 
     height, width, _ = image.shape
+
+    if 0 in image.shape:
+        return None
 
     if height < width:
 
@@ -17,20 +20,23 @@ def random_square_crop_with_resize(
         image_resized = cv2.resize(
             image, (new_width, new_height), interpolation=interpolation
         )
-
-        extra_width = new_width - new_side_len
-        margin = extra_width // 2
-        rand_adjustment = random.randint(0, margin) * random.choice([1, -1])
-
-        if margin % 2 == 1:
-            shift = 1
+        if image_resized.shape[0] == image_resized.shape[1]:
+            image_square = image_resized
         else:
-            shift = 0
+            extra_width = new_width - new_side_len
+            margin = extra_width // 2
+            rand_adjustment = random.randint(0, margin) * random.choice([1, -1])
 
-        image_square = image_resized[
-            :, margin - rand_adjustment : -(margin + shift + rand_adjustment), :
-        ]
+            if margin % 2 == 1:
+                shift = 1
+            else:
+                shift = 0
 
+            image_square = image_resized[
+                :, margin - rand_adjustment : -(margin + shift + rand_adjustment), :
+            ]
+
+        
     if width < height:
 
         new_width = new_side_len
@@ -39,29 +45,31 @@ def random_square_crop_with_resize(
         image_resized = cv2.resize(
             image, (new_width, new_height), interpolation=interpolation
         )
-
-        extra_height = new_height - new_side_len
-        margin = extra_height // 2
-        rand_adjustment = random.randint(0, margin) * random.choice([1, -1])
-
-        if margin % 2 == 1:
-            shift = 1
+        if image_resized.shape[0] == image_resized.shape[1]:
+            image_square = image_resized
         else:
-            shift = 0
+            extra_height = new_height - new_side_len
+            margin = extra_height // 2
+            rand_adjustment = random.randint(0, margin) * random.choice([1, -1])
 
-        image_square = image_resized[
-            margin - rand_adjustment : -(margin + shift + rand_adjustment), :, :
-        ]
+            if margin % 2 == 1:
+                shift = 1
+            else:
+                shift = 0
 
-    if width == height:
-        image_square = cv2.resize(
-            image, (new_side_len, new_side_len), interpolation=interpolation
-        )
+            image_square = image_resized[
+                margin - rand_adjustment : -(margin + shift + rand_adjustment), :, :
+            ]
 
+        if width == height:
+            image_square = cv2.resize(
+                image, (new_side_len, new_side_len), interpolation=interpolation
+            )
+#         print('h=w')
     return image_square
 
 
-def rotate_and_zoom(image):
+def rotate_and_zoom(image, image_name):
     # image must be square
     s = image.shape[0]
 
@@ -71,83 +79,103 @@ def rotate_and_zoom(image):
     try:
     	image = cv2.warpAffine(image, rotation_matrix, (s, s))
     except:
-    	print(s, ang)
+    	print('rot: ', s, ang, image_name)
 
     # TODO: Explain this
     # 1.41 ~ root(2), this sin func maxes at 45 deg, s//8 found empircally
     x = abs(int(s//8 * np.sin(np.deg2rad(45 + ang // 45 + ang % 45)) * 1.41))
     image = image[x:-x, x:-x, :]
 
-    image = cv2.resize(image, (s, s), interpolation=cv2.INTER_LANCZOS4)
+    try:
+        image = cv2.resize(image, (s, s), interpolation=cv2.INTER_LANCZOS4)
+    except:
+        print('rot: ', s, x, ang, image_name)
     return image
 
 
-def adjust_contrast(image):
+def adjust_contrast(image, image_name,):
     alpha = 0.5 + random.random()
-    image = cv2.convertScaleAbs(image, alpha=alpha, beta=0)
+    try:
+        image = cv2.convertScaleAbs(image, alpha=alpha, beta=0)
+    except:
+        print('cont: ', image_name, alpha)
     return image
 
 
-def adjust_brightness(image):
+def adjust_brightness(image, image_name,):
     beta = random.random() * 100
-    image = cv2.convertScaleAbs(image, alpha=1, beta=beta)
+    try:
+        image = cv2.convertScaleAbs(image, alpha=1, beta=beta)
+    except:
+        print('brit: ', image_name, beta)
     return image
 
 
-def adjust_saturation(image):
+def adjust_saturation(image, image_name,):
     satadj = random.random() * 2
-    imghsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV).astype("float32")
-    h, s, v = cv2.split(imghsv)
-    s = s*satadj
-    s = np.clip(s,0,255)
-    imghsv = cv2.merge([h,s,v])
-    image = cv2.cvtColor(imghsv.astype("uint8"), cv2.COLOR_HSV2BGR)
+    try:
+        imghsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV).astype("float32")
+        h, s, v = cv2.split(imghsv)
+        s = s*satadj
+        s = np.clip(s,0,255)
+        imghsv = cv2.merge([h,s,v])
+        image = cv2.cvtColor(imghsv.astype("uint8"), cv2.COLOR_HSV2BGR)
+    except:
+        print('sat: ', image_name, satadj)
     return image
 
 
-def flip_left_right(img):
-    return np.fliplr(img)
-
-def noisy(image):
-
-    noise_typ = random.choice(['gauss', 's&p', 'poisson', 'speckle'])
-
-# def noisy_1(noise_typ, image):
-
-    if noise_typ == "gauss":
-        
-        row, col, ch = image.shape
-        mean = 0
-        var = 1
-        sigma = var ** 0.5
-        gauss = np.random.normal(mean, sigma, image.shape)
-        gauss = gauss.reshape(row, col, ch)
-        noisy_img = image + gauss
-        return noisy_img.astype('uint8')
+def flip_left_right(img, image_name,):
     
-    elif noise_typ == "s&p":
-        prob = 0.005
-#     def add_salt_and_pepper(gb, prob):
-        '''Adds "Salt & Pepper" noise to an image.
-        gb: should be one-channel image with pixels in [0, 1] range
-        prob: probability (threshold) that controls level of noise'''
+    try:
+        img = np.fliplr(img)
+    except:
+        print('lr: ', image_name)
+    return img
 
-        rnd = np.random.rand(image.shape[0], image.shape[1], image.shape[2])
-        noisy_img = image.copy()
-        noisy_img[rnd < prob] = 0
-        noisy_img[rnd > 1 - prob] = 255
-        return noisy_img
-    elif noise_typ == "poisson":
-        vals = len(np.unique(image))
-        vals = 2 ** np.ceil(np.log2(vals))
-        noisy_img = np.random.poisson(image * vals) / float(vals)
-        return noisy_img.astype('uint8')
-    elif noise_typ == "speckle":
-        row, col, ch = image.shape
-        gauss = np.random.randn(row, col, ch) / 255
-        gauss = gauss.reshape(row, col, ch)
-        noisy_img = image + image * gauss
-        return noisy_img.astype('uint8')
+def noisy(image, image_name,):
+    try:
+
+        noise_typ = random.choice(['gauss', 's&p', 'poisson', 'speckle'])
+
+    # def noisy_1(noise_typ, image):
+
+        if noise_typ == "gauss":
+            
+            row, col, ch = image.shape
+            mean = 0
+            var = 1
+            sigma = var ** 0.5
+            gauss = np.random.normal(mean, sigma, image.shape)
+            gauss = gauss.reshape(row, col, ch)
+            noisy_img = image + gauss
+            return noisy_img.astype('uint8')
+        
+        elif noise_typ == "s&p":
+            prob = 0.005
+    #     def add_salt_and_pepper(gb, prob):
+            '''Adds "Salt & Pepper" noise to an image.
+            gb: should be one-channel image with pixels in [0, 1] range
+            prob: probability (threshold) that controls level of noise'''
+
+            rnd = np.random.rand(image.shape[0], image.shape[1], image.shape[2])
+            noisy_img = image.copy()
+            noisy_img[rnd < prob] = 0
+            noisy_img[rnd > 1 - prob] = 255
+            return noisy_img
+        elif noise_typ == "poisson":
+            vals = len(np.unique(image))
+            vals = 2 ** np.ceil(np.log2(vals))
+            noisy_img = np.random.poisson(image * vals) / float(vals)
+            return noisy_img.astype('uint8')
+        elif noise_typ == "speckle":
+            row, col, ch = image.shape
+            gauss = np.random.randn(row, col, ch) / 255
+            gauss = gauss.reshape(row, col, ch)
+            noisy_img = image + image * gauss
+            return noisy_img.astype('uint8')
+    except:
+        print(noise_typ, image_name)
 
 
 
@@ -167,4 +195,4 @@ from tools import new_image_name
 #     image = random_square_crop_with_resize(image)
 #     image_name = new_image_name(get_image_name_from_path(fn), "a")
 #     cv2.imwrite("../teed/" + image_name, image)
-#     sys.stdout.write("\r{:2.2f}%".format(100 * (i + 1) / len(fns)))
+#     print("\r{:2.2f}%".format(100 * (i + 1) / len(fns)))
