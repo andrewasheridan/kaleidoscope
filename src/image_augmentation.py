5d1b111e4b782f33cf9b98cc081f4519b7b5ac2a
Andrew Sheridan <sheridan@berkeley.edu>
2019-01-28 13:52:13 -0500

    Moved IA init code to BaseIA

--- src/image_augmentation.py
+++ src/image_augmentation.py
@@ -1,5 +1,9 @@
-"""Summary
+"""image_augmentation.py
+    
+    Holds image augmentation classes, but not trasformations (see transoformations.py)
 """
+
+# TODO: Cleanup Imports
 from numpy import ndarray, sin, deg2rad
 from string import ascii_lowercase
 from transformations import random_square_crop_with_resize
@@ -15,18 +19,8 @@ import cv2
 import time
 
 
-def image_as_ndarray(image):
-    # TODO: Add conversion to ndarray when possible
-    if type(image) is ndarray:
-        image = random_square_crop_with_resize(image)
-    else:
-        raise TypeError(
-            "`image` must be convertible to array not {}".format(type(image))
-        )
-    return image
-
-
 class BaseImageAugmenter(object):
+
     def __init__(self, image_name, num_transformations, save=True):
         """Base class for ImageAugmenter
         
@@ -39,7 +33,93 @@ class BaseImageAugmenter(object):
         save : bool, optional, default=True
             For debugging - Anything not saved is lost
         """
-        self.image = image_as_ndarray(image)
+        # TODO: Add conversion to ndarray when possible
+        if type(image) is ndarray:
+            self.image = random_square_crop_with_resize(image)
+        else:
+            raise TypeError(
+                "`image` must be np.ndarray not {}".format(type(image))
+            )
+
+        # TODO: Add conversino to str when possible
+        if type(image_name) is str:
+            self._image_name = image_name
+        else:
+            raise TypeError("`image_name` must be str not {}".format(type(image_name)))
+
+        # TODO: Pass in preset list of transformations instead of a list
+        # Only have 6 transformations currently
+        if num_transformations <= 6:
+            self._num_transformations = num_transformations
+        else:
+            raise ValueError("No more than 6 transformations possible.")
+
+        self._aug_image_names = self._generate_augmented_image_names()
+        self._transformations = self._generate_transformations()
+
+    def _generate_transformations(self):
+        """Summary
+        
+        Returns
+        -------
+        TYPE
+            Description
+        """
+        n = self._num_transformations
+        rand_tees = [
+            rotate_and_zoom,
+            adjust_contrast,
+            adjust_brightness,
+            adjust_saturation,
+            flip_left_right,
+            noisy,
+        ]
+        shuffle(rand_tees)
+        transformations = {ascii_lowercase[i]: rand_tees[i] for i in range(0, n)}
+        return transformations
+
+        # print(self._aug_image_names)
+
+    def _generate_augmented_image_names(self):
+        """Summary
+        
+        Returns
+        -------
+        TYPE
+            Description
+        """
+        words_to_append = self._generate_chars_to_append()
+        name, extension = self._image_name.split(".")
+        aug_image_names = [name + word + "." + extension for word in words_to_append]
+        aug_image_names.sort(key=lambda item: (len(item), item))
+        return aug_image_names
+
+    # TODO: Explain how this works and provide example
+    # TODO: Find a slicker way
+    def _generate_chars_to_append(self):
+        """Summary
+        
+        Returns
+        -------
+        TYPE
+            Description
+        """
+        # NOTE: there is *surely* a slicker way to do this
+        # trying to do https://stackoverflow.com/a/16241785/10918177
+        # but with letters
+        n = self._num_transformations
+        words = []
+
+        for i in range(1, 1 << n):
+            s = "{0:0{1:d}b}".format(i, n)
+            word = ""
+
+            for j in range(0, n):
+                if s[j] == "1":
+                    word += ascii_lowercase[j]
+
+            words.append(word)
+        return words
 
 
 class ImageAugmenter(object):
@@ -81,85 +161,12 @@ class ImageAugmenter(object):
             save=save,
         )
         
-
-        if type(image_name) is str:
-            self._image_name = image_name
-        else:
-            raise TypeError("`image_name` must be str not {}".format(type(image_name)))
-
-        # Limiting the number of transformations because of the simple naming convention
-        if num_transformations <= 6:
-            self._num_transformations = num_transformations
-        else:
-            raise ValueError("No more than 6 transformations possible.")
-
-        self._aug_image_names = self._generate_augmented_image_names()
-        self._transformations = self._generate_transformations()
+        # TODO: Change behavior to calling transform and save directly
         self._images = self.transform()
         if save:
             self.save()
 
-    def _generate_transformations(self):
-        """Summary
-        
-        Returns
-        -------
-        TYPE
-            Description
-        """
-        n = self._num_transformations
-        rand_tees = [
-            rotate_and_zoom,
-            adjust_contrast,
-            adjust_brightness,
-            adjust_saturation,
-            flip_left_right,
-            noisy,
-        ]
-        shuffle(rand_tees)
-        transformations = {ascii_lowercase[i]: rand_tees[i] for i in range(0, n)}
-        return transformations
 
-        # print(self._aug_image_names)
-
-    def _generate_augmented_image_names(self):
-        """Summary
-        
-        Returns
-        -------
-        TYPE
-            Description
-        """
-        words_to_append = self._generate_chars_to_append()
-        name, extension = self._image_name.split(".")
-        aug_image_names = [name + word + "." + extension for word in words_to_append]
-        aug_image_names.sort(key=lambda item: (len(item), item))
-        return aug_image_names
-
-    def _generate_chars_to_append(self):
-        """Summary
-        
-        Returns
-        -------
-        TYPE
-            Description
-        """
-        # NOTE: there is *surely* a slicker way to do this
-        # trying to do https://stackoverflow.com/a/16241785/10918177
-        # but with letters
-        n = self._num_transformations
-        words = []
-
-        for i in range(1, 1 << n):
-            s = "{0:0{1:d}b}".format(i, n)
-            word = ""
-
-            for j in range(0, n):
-                if s[j] == "1":
-                    word += ascii_lowercase[j]
-
-            words.append(word)
-        return words
 
     def transform(self):
         """Summary
@@ -181,11 +188,11 @@ class ImageAugmenter(object):
 
         return images
 
-    def save(self):
+    # TODO: Change save from local to S3
+    def save(self, dir="../auger/"):
         """Summary
         """
         for image_name in self._images:
-            dir = "../auger/"
             cv2.imwrite(dir + image_name, self._images[image_name])
             cv2.imwrite(dir + self._image_name, self.image)
 
