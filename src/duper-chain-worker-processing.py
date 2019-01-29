433e5c01035970af83fee1b5ff5c02024e2a605b
Andrew Sheridan <sheridan@berkeley.edu>
2019-01-29 13:01:18 -0500

    created worker processing script

---
+++ src/duper-chain-worker-processing.py
@@ -0,0 +1,12 @@
+
+
+# recieve image_paths.pickle
+
+# open image_paths.pickle 
+
+# for each path
+#   load image, augment & save to s3 (or maybe save locally and then push to s3 en masse)
+#   load tagline, create new taglines, write taglines locally, p
+
+
+# rec
