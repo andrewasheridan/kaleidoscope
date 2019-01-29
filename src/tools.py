11f59485bd8f7aee6899f0501621fee3677c4c9c
Andrew Sheridan <sheridan@berkeley.edu>
2019-01-29 18:22:08 -0500

    moved tools

--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,7 @@
+import os
 import pickle
 
+from constants import S3_KEYS_DIR
 from itertools import takewhile
 
 
@@ -18,8 +20,14 @@ def new_image_name(base_image_name, char):
     return new_image_name
 
 
-def save_obj(obj, filename, dir="s3_keys/"):
+def make_dir_if_DNE(dir):
+    """Make dir if it does not exist"""
+    os.makedirs(os.path.dirname(dir), exist_ok=True)
+
+
+def save_obj(obj, filename, dir=S3_KEYS_DIR):
     """filename does not include `.pickle` extensions""" 
+    make_dir_if_DNE(dir)
     with open(dir + filename + ".pickle", "wb") as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
@@ -27,3 +35,41 @@ def save_obj(obj, filename, dir="s3_keys/"):
 def load_obj(path):
     with open(path, "rb") as f:
         return pickle.load(f)
+
+
+def bucket_exists():                                                                                                                                                                                                                                                                                                                                                                                                        
+    s3 = boto3.resource("s3")
+    return s3.Bucket(bucketname) in s3.buckets.all()
+
+
+def get_max_filename_in_dir_and_increment(dir, prefix="batch_"):
+
+    try:
+        current_max_filename = max(glob.glob(dir + prefix + "*"))
+    except ValueError:
+        current_max_filename = None
+
+    if current_max_filename:
+
+        current_max_filename = current_max_filename.replace("_", " ")
+        current_max_filename = current_max_filename.replace(".", " ")
+        current_max_number = [
+            int(s) for s in current_max_filename.split() if s.isdigit()
+        ][0]
+        new_number = "{0:0=10d}".format(current_max_number + 1)
+
+    else:
+
+        current_max_number = 0
+        new_number = "{0:0=10d}".format(current_max_number)
+
+    new_filename = prefix + new_number
+
+    return new_filename
+
+
+
+
+
+
+    
