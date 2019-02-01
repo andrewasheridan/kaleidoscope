70d23b2f0729980db22de8b9e0e7bf8332c41498
Andrew Sheridan <sheridan@berkeley.edu>
2019-02-01 15:22:55 -0500

    formatting changes

--- src/duper_chains_object_scraping.py
+++ src/duper_chains_object_scraping.py
@@ -2,9 +2,9 @@ import boto3
 import constants
 import tools
 
+
 class S3ObjectRetrievalBase(object):
-    def __init__(self,
-        bucketname):
+    def __init__(self, bucketname):
 
         self.bucketname = bucketname
         try:
@@ -12,7 +12,12 @@ class S3ObjectRetrievalBase(object):
                 self.s3 = boto3.client("s3")
         except:
             print()
-            raise ValueError("Bucket error: {} does not exist".format(self.bucketname))
+            raise ValueError(
+                "Bucket error: {} does not exist (or something else)".format(
+                    self.bucketname
+                )
+            )
+
 
 class S3ObjectRetrieval(S3ObjectRetrievalBase):
     def __init__(self, bucketname):
@@ -20,9 +25,10 @@ class S3ObjectRetrieval(S3ObjectRetrievalBase):
 
     def _save_batch_keys(self):
 
-        tools.get_max_filename_in_dir_and_increment(dir=constants.S3_KEYS_DIR, prefix=constants.METADATA_BATCH_PREFIX)
+        tools.get_max_filename_in_dir_and_increment(
+            dir=constants.S3_KEYS_DIR, prefix=constants.METADATA_BATCH_PREFIX
+        )
         tools.save_obj(batch_keys)
-
 
     def scrape_s3_metadata(self):
 
@@ -33,11 +39,12 @@ class S3ObjectRetrieval(S3ObjectRetrievalBase):
             response = self.s3.list_objects_v2(**kwargs)
             try:
                 contents = response["Contents"]
+
+                # TODO: replace with add_to_redis_queue
                 self._save_batch_keys(contents)
 
             except KeyError:
                 return
-
 
             for obj in contents:
                 yield obj
@@ -47,5 +54,3 @@ class S3ObjectRetrieval(S3ObjectRetrievalBase):
 
             except KeyError:
                 break
-
-
