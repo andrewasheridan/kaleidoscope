0f5d645c28db5d0ca33401fa5dddf58609453f80
Andrew Sheridan <sheridan@berkeley.edu>
2019-02-03 14:31:21 -0500

    changed save_batch to put(pickled)

--- src/duper_chains_object_scraping.py
+++ src/duper_chains_object_scraping.py
@@ -1,6 +1,7 @@
 import boto3
 import constants
 import tools
+import pickle
 
 
 class S3ObjectRetrievalBase(object):
@@ -20,19 +21,21 @@ class S3ObjectRetrievalBase(object):
 
 
 class S3ObjectRetrieval(S3ObjectRetrievalBase):
-    def __init__(self, bucketname):
-        S3ObjectRetrievalBase.__init__(self, bucketname=bucketname)
+    def __init__(self, bucketname, queue):
+        S3ObjectRetrievalBase.__init__(self,
+        bucketname=bucketname)
+        self.queue = queue
 
-    def _save_batch_keys(self):
+    # def _save_batch_keys(self):
 
-        tools.get_max_filename_in_dir_and_increment(
-            dir=constants.S3_KEYS_DIR, prefix=constants.METADATA_BATCH_PREFIX
-        )
-        tools.save_obj(batch_keys)
+    #     tools.get_max_filename_in_dir_and_increment(
+    #         dir=constants.S3_KEYS_DIR, prefix=constants.METADATA_BATCH_PREFIX
+    #     )
+    #     tools.save_obj(batch_keys)
 
     def scrape_s3_metadata(self):
-
-        kwargs = {"Bucket": bucket}
+        print("scrape_s3_metadata")
+        kwargs = {"Bucket": self.bucketname}
 
         while True:
 
@@ -41,13 +44,16 @@ class S3ObjectRetrieval(S3ObjectRetrievalBase):
                 contents = response["Contents"]
 
                 # TODO: replace with add_to_redis_queue
-                self._save_batch_keys(contents)
+                # self._save_batch_keys(contents)
+                pickled_batch = pickle.dumps(contents)
+                self.queue.put(pickled_batch)
+                print("self.queue.put")
 
             except KeyError:
                 return
 
-            for obj in contents:
-                yield obj
+            # for obj in contents:
+            #     yield obj
 
             try:
                 kwargs["ContinuationToken"] = response["NextContinuationToken"]
