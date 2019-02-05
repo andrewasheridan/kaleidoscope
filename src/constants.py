009b7fe81f9de9764a8f0dd6263aab1e1ae85137
Andrew Sheridan <sheridan@berkeley.edu>
2019-02-05 11:29:51 -0500

    removed unused constants. added comment for OPENCV_FILETYPES. added banner and file description.

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,20 @@
-# TODO: Confirm each filetype is supported in container
+#                      _              _       
+#                     | |            | |      
+#   ___ ___  _ __  ___| |_ __ _ _ __ | |_ ___ 
+#  / __/ _ \| '_ \/ __| __/ _` | '_ \| __/ __|
+# | (_| (_) | | | \__ \ || (_| | | | | |_\__ \
+#  \___\___/|_| |_|___/\__\__,_|_| |_|\__|___/
+#
+
+"""
+constants.py
+
+various fixed values used elsewhere
+"""
+
+
+# available OpenCV image file types
+# loaded images should be checked that their format is in this list
 # docs.opencv.org/3.0-beta/modules/imgcodecs/doc/reading_and_writing_images.html#imread
 OPENCV_FILETYPES = [
     ".bmp",
@@ -18,6 +34,6 @@ OPENCV_FILETYPES = [
     ".tif",
 ]
 
-S3_KEYS_DIR = "../s3_keys/"
-
-METADATA_BATCH_PREFIX = "batch_"
+# S3_KEYS_DIR = "../s3_keys/"
+#
+# METADATA_BATCH_PREFIX = "batch_"
