dbdce90b43fa257b87d90b703eba79a93d68cbae
Andrew Sheridan <sheridan@berkeley.edu>
2019-02-05 11:53:39 -0500

    removed debugging print statements, unused imports

--- src/queue_maker.py
+++ src/queue_maker.py
@@ -1,40 +1,11 @@
 #!/usr/bin/env python
-print("queue_maker.py")
 import duper_chains_object_scraping
-import pickle
 import rediswq
-import time
-import tools
 
-# host = "redis"
-# # Uncomment next two lines if you do not have Kube-DNS working.
-# # import os
-# # host = os.getenv("REDIS_SERVICE_HOST")
 
 queue = rediswq.RedisWQ(name="job2", host="redis")
-# print("Worker with sessionID: " + q.sessionID())
-# print("Initial queue state: empty=" + str(q.empty()))
-print("QM:rediswq.RedisWQ created")
-
 scraper = duper_chains_object_scraping.S3ObjectRetrieval(
     bucket_name="chainsaw-dogs-and-cats",
-    queue = queue,
+    queue=queue,
 )
-# s3_keys = 
 scraper.scrape_s3_metadata()
-print('QM:done')
-# print("Total number of keys = " + len(s3_keys))
-# for i in range(100):
-
-#     # print(i)
-#     time.sleep(3)
-
-# batch = [i+j**2 for j in range(5)]
-# time.sleep(15)
-# pickled_batch = pickle.dumps(batch)
-# print(batch)
-
-# q.put(pickled_batch)
-
-
-#
