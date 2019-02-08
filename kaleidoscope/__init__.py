ffc37fd19243f81170a072f194a0fa0ba528d37f
Andrew Sheridan <sheridan@berkeley.edu>
2019-02-08 16:50:51 -0500

    added more to init

--- kaleidoscope/__init__.py
+++ kaleidoscope/__init__.py
@@ -1,1 +1,11 @@
-from kaleidoscope.interface import Cluster
+from interface import Cluster
+from create_job_yaml import create_job_yaml
+from create_queue_maker_yaml import create_queue_maker_yaml
+from create_secret_yaml import create_secret_yaml
+
+__all__ = [
+    'Cluster',
+    'create_job_yaml',
+    'create_queue_maker_yaml',
+    'create_secret_yaml',
+]
