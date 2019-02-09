d86f015bb15d6d0a7276ec7416bc732c6c355cfd
Andrew Sheridan <sheridan@berkeley.edu>
2019-02-09 17:23:38 -0500

    expanded bucket creation and cluster status chagnes

--- kaleidoscope/_interface.py
+++ kaleidoscope/_interface.py
@@ -3,6 +3,15 @@ import os
 import subprocess
 import sys
 
+from kaleidoscope.create_secret_yaml import create_secret_yaml
+from kaleidoscope.create_secret_store_yaml import create_secret_store_yaml
+from kaleidoscope.create_redis_service_yaml import create_redis_service_yaml
+from kaleidoscope.create_redis_master_yaml import create_redis_master_yaml
+from kaleidoscope.create_queue_maker_yaml import create_queue_maker_yaml
+
+
+# import kaleidoscope.create_secret_yaml as create_secret_yaml
+# import kaleidoscope.create_secret_store_yaml as create_secret_store_yaml
 # cluster = Cluster("kaleidoscope")
 # "Cluster not ready. If you have previously created a cluster, please wait longer try again."
 # "If you have not created a cluster, please do Cluster.create_cluster()"
@@ -16,16 +25,21 @@ import sys
 # <10 minutes later>
 # cluster. cluster_ready()
 # "cluster ready -
+# TODO: validate_cluster()
 
 
 class Cluster(object):
-    def __init__(self, cluster_name_prefix='kaleidoscope'):
+    def __init__(self, cluster_name_prefix='kaleidoscope', verbose=True):
+
+        self._vprint = sys.stdout.write if verbose else lambda *a, **k: None
 
         self.cluster_name_prefix = cluster_name_prefix
         self.cluster_name = self.cluster_name_prefix + ".k8s.local"
         self.kops_state_store_name = self.cluster_name_prefix + "-kops-state-store"
+        self.original_images_bucket = self.cluster_name_prefix + "-original-images-bucket"
+        self.augmented_images_bucket = self.cluster_name_prefix + "-augmented-images-bucket"
 
-        # TODO: Find way to install homebrew, kops, kubectl via pip / setup.py
+        # TODO: Find way to install homebrew, kops, kubectl via pip / setup.py, maybe?
         # kubectl and kops need to be installed via homebrew
         if not self._package_message_non_zero("kubectl", " -h"):
             install_command = "`brew update && brew install kops kubectl`"
@@ -43,36 +57,55 @@ class Cluster(object):
 
         self._check_environmental_variables()
 
+        # TODO: Test if cluster created (but how?)
         if not self._cluster_ready():
-            print("Cluster not ready. If you have created a cluster, please wait longer try again.")
-            print("If you have not created a cluster, please do Cluster.create_cluster()")
+            self._vprint("\rCluster not ready. If you have created a cluster, please wait longer try again."
+                         + " If you have not created a cluster, please do Cluster.create_cluster()")
+
+        # need a place to store original images
+        if not self._bucket_exists(self.original_images_bucket):
+            self._create_bucket(self.original_images_bucket)
+
+        # need a place to store augmented images
+        if not self._bucket_exists(self.augmented_images_bucket):
+            self._create_bucket(self.augmented_images_bucket)
 
     def _package_message_non_zero(self, package, args):
         """Checks if a package exists in this environment by finding the output length of a message"""
+        # TODO: Make this more robust
+        self._vprint(f"\r{package}: checking installation")
         command = package + args
         output = self._pass_command_to_shell(command)
 
-        # TODO: Make this more robust
         return bool(len(output))
 
-    @staticmethod
-    def _bucket_exists(bucket_name):
+    def _bucket_exists(self, bucket_name):
+        self._vprint(f"\r{bucket_name}: Checking existence")
         s3 = boto3.resource("s3")
-        return s3.Bucket(bucket_name) in s3.buckets.all()
+        return s3.Bucket(self.bucket_name) in s3.buckets.all()
 
-    def _create_cluster_state_store(self):
+    @staticmethod
+    def _create_bucket(bucket_name):
         create = "aws s3api create-bucket --bucket "\
-                 + self.kops_state_store_name\
+                 + bucket_name\
                  + " --region us-east-1"
 
         subprocess.run(create.split())
+
+    def _create_cluster_state_store(self):
+        self._vprint(f"\r{self.kops_state_store_name}: Not found - Creating")
+        self._create_bucket(self.kops_state_store_name)
 
         apply_versioning = "aws s3api put-bucket-versioning --bucket "\
                            + self.kops_state_store_name\
                            + " --versioning-configuration Status=Enabled"
         subprocess.run(apply_versioning.split())
 
+        # TODO: confirm existence of state store
+        self._vprint(f"\r{self.kops_state_store_name}: Should be created")
+
     def _check_environmental_variables(self):
+        self._vprint("\rEnvironment variables: confirming")
         message = "please run this command in Terminal and try again:"
         if os.environ["KOPS_CLUSTER_NAME"] != f"{self.cluster_name}":
             message += f"""'export KOPS_CLUSTER_NAME="{self.cluster_name}"' >> $HOME/.bash_profile"""
@@ -81,16 +114,19 @@ class Cluster(object):
         if os.environ["KOPS_STATE_STORE"] != f"s3://{self.kops_state_store_name}":
             message += f"""'export KOPS_STATE_STORE="s3://{self.kops_state_store_name}"' >> $HOME/.bash_profile"""
             raise EnvironmentError(message)
+        self._vprint("\rEnvironment variables: confirmed")
 
     def _cluster_ready(self):
         """Checks if a package exists in this environment by finding the output length of a message"""
-        output = self._pass_command_to_shell("kops validate cluster")
+        command = "kops validate cluster"
+        self._vprint(f"\r$: {command}")
+        output = self._pass_command_to_shell(command)
         message = f"Your cluster {self.cluster_name} is ready"
-        sys.stdout.write(output)
+        # sys.stdout.write(output)
         return message in output
 
-    def cluster_ready(self):
-        self._cluster_ready()
+    def validate_cluster(self):
+        return self._cluster_ready()
 
     @staticmethod
     def _pass_command_to_shell(command):
@@ -99,24 +135,42 @@ class Cluster(object):
         output = output.decode().strip()
         return output
 
-    def _spin_up_cluster(self, dry_run=True):
-        arg = " --yes" if dry_run else ""
+    def _spin_up_cluster(self, dry_run=True, raw_output=False):
+        arg = " --yes" if not dry_run else ""
         command = f"kops update cluster --name {self.cluster_name}{arg}"
-        sys.stdout.write(self._pass_command_to_shell(command))
-        print("spinning up cluster, this may take some time (5+ minutes")
+        self._vprint(f"\r$: {command}")
+        output = self._pass_command_to_shell(command)
+        if raw_output:
+            print(output)
+        self._vprint("\rSpinning up cluster, this may take some time (5+ minutes")
 
-    def create_cluster(self, node_count=10, node_size="m4.large", region="us-east-1a", dry_run=True):
+    def create_cluster(self, node_count=10, node_size="m4.large", region="us-east-1a", dry_run=False, raw_output=False):
         command = f"kops create cluster --node-count={node_count} --node-size={node_size} --zones={region}"
-        sys.stdout.write(self._pass_command_to_shell(command))
-        self._spin_up_cluster(dry_run=dry_run)
+        self._vprint(f"\r$: {command}")
+        output = self._pass_command_to_shell(command)
+        if raw_output:
+            print(output)
+        self._spin_up_cluster(dry_run=dry_run,raw_output=raw_output)
 
-    def delete_cluster(self, dry_run=True):
-        arg = " --yes" if dry_run else ""
+    def delete_cluster(self, dry_run=False, raw_output=False):
+        arg = " --yes" if not dry_run else ""
         command = f"kops delete cluster{arg}"
-        self._pass_command_to_shell(command)
+        self._vprint(f"\r$: {command}")
+        output = self._pass_command_to_shell(command)
+        if raw_output:
+            print(output)
+        self._vprint(f"\rCluster deleted")
 
+    @staticmethod
+    def configure_base_cluster(self):
+        create_secret_yaml()
+        create_secret_store_yaml()
+        create_redis_service_yaml()
+        create_redis_master_yaml()
 
+    def upload(self, local_images_directory):
 
-    def build(self):
-        print("Building " + self.cluster_name_prefix)
+        command = f"aws s3 cp {local_images_directory} s3://{self.kops_state_store_name} --recursive --quiet"
+        self._pass_command_to_shell()
+        os.system()
 
