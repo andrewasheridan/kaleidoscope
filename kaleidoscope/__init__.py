c7b4599e75aea51e830b5cbf607f81f90447e4b1
Andrew Sheridan <sheridan@berkeley.edu>
2019-02-10 12:41:56 -0500

    cleaned up init

--- kaleidoscope/__init__.py
+++ kaleidoscope/__init__.py
@@ -1,11 +1,5 @@
-from ._interface import Cluster
-from .create_job_yaml import create_job_yaml
-from .create_queue_maker_yaml import create_queue_maker_yaml
-from .create_secret_yaml import create_secret_yaml
+from ._interface import Interface
 
 __all__ = [
-    'Cluster',
-    'create_job_yaml',
-    'create_queue_maker_yaml',
-    'create_secret_yaml',
+    'Interface',
 ]
