606f8a1e0f4283d51da2c545f03074726f3bdf39
Andrew Sheridan <sheridan@berkeley.edu>
2019-02-10 12:40:14 -0500

    moved yaml creation code to _create_yamls module

---
+++ kaleidoscope/_create_yamls/__init__.py
@@ -0,0 +1,15 @@
+from ._create_secret_yaml import create_secret_yaml as _create_secret_yaml
+from ._create_secret_store_yaml import create_secret_store_yaml as _create_secret_store_yaml
+from ._create_redis_service_yaml import create_redis_service_yaml as _create_redis_service_yaml
+from ._create_redis_master_yaml import create_redis_master_yaml as _create_redis_master_yaml
+from ._create_queue_maker_yaml import create_queue_maker_yaml as _create_queue_maker_yaml
+from ._create_job_yaml import create_job_yaml as _create_job_yaml
+
+__all__ = [
+    '_create_secret_yaml',
+    '_create_secret_store_yaml',
+    '_create_redis_service_yaml',
+    '_create_redis_master_yaml',
+    '_create_queue_maker_yaml',
+    '_create_job_yaml'
+]
