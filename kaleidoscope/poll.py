5252efa67581aaf26c15af8f05b18888bbb5a5e0
Andrew Sheridan <sheridan@berkeley.edu>
2019-02-11 14:45:01 -0500

    added docstring, made constants explicit, moved poll code into function

--- kaleidoscope/poll.py
+++ kaleidoscope/poll.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python
+"""
+              _ _
+             | | |
+  _ __   ___ | | |
+ | '_ \ / _ \| | |
+ | |_) | (_) | | |
+ | .__/ \___/|_|_|
+ | |
+ |_|
+"""
 import rediswq
 import time
-import sys
 
-host = "redis"
-queue = rediswq.RedisWQ(name="job2", host="redis")
+HOST = "redis"
+JOB_NAME = "job2"
+PRINT_DELAY = 5
 
-while not queue.empty():
+queue = rediswq.RedisWQ(name=JOB_NAME, host=HOST)
 
-    main_q_size, _ = queue.get_queue_sizes
-    time.sleep(5)
-    print(f"{main_q_size}")
+
+def poll(queue, print_delay):
+    """
+    poll checks the number of remaining items in the redis queue and prints the value.
+    This value is retrieved from the pod named `poll` using `kubectl logs poll`.
+
+    :param queue: RedisWQ() - object that has some items in its database
+    :param print_delay: int - a delay between prints so the output is not overwhelming
+    :return: None
+    """
+    while not queue.empty():
+
+        main_q_size, _ = queue.get_queue_sizes
+        time.sleep(print_delay)
+        print(f"{main_q_size}")
+
+    return None
+
+
+poll(queue, PRINT_DELAY)
