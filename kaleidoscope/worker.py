2653e54dbd7ad42782146f387cf6420b553c8312
Andrew Sheridan <sheridan@berkeley.edu>
2019-02-12 10:01:10 -0500

    cleaned up worker, moved code into function

--- kaleidoscope/worker.py
+++ kaleidoscope/worker.py
@@ -1,43 +1,65 @@
 #!/usr/bin/env python
+#                      _
+#                     | |
+#  __      _____  _ __| | _____ _ __
+#  \ \ /\ / / _ \| '__| |/ / _ \ '__|
+#   \ V  V / (_) | |  |   <  __/ |
+#    \_/\_/ \___/|_|  |_|\_\___|_|
+"""
+worker.py
+
+Script to be installed in the job pods created using `job.yaml`
+"""
 import boto3
 import cv2
 import constants
 import os
 import pickle
-import time
 import rediswq
 
 from image_augmenter import KaleidoscopeAugmenter
 
-host = "redis"
+HOST = "redis"
+JOB_NAME = "job2"
 
+# This environmental variable is set in the Docker Container the script runs inside of.
+# It is set when the Pod is created with `kubectl create -f job.yaml` or during Interface().transform()
 ORIGIN_S3 = os.environ['ORIGIN_S3']
-queue = rediswq.RedisWQ(name="job2", host="redis")
-s3 = boto3.resource('s3', region_name=constants.AWS_REGION)
-origin_bucket = s3.Bucket(ORIGIN_S3)
 
-os.makedirs(os.path.dirname(constants.DOWNLOAD_DIRECTORY), exist_ok=True)
+queue = rediswq.RedisWQ(name=JOB_NAME, host=HOST)
 
-while not queue.empty():
-    start = time.time()
-    item = queue.lease(lease_secs=10, block=True, timeout=2)
 
-    if item is not None:
-        batch = pickle.loads(item)
+def worker(queue):
+    s3 = boto3.resource('s3', region_name=constants.AWS_REGION)
+    origin_bucket = s3.Bucket(ORIGIN_S3)
 
-        for i, key in enumerate(batch):
+    os.makedirs(os.path.dirname(constants.DOWNLOAD_DIRECTORY), exist_ok=True)
 
-            path = constants.DOWNLOAD_DIRECTORY + key["Key"]
-            os.makedirs(os.path.dirname(path), exist_ok=True)
-            origin_bucket.download_file(key["Key"], path)
+    while not queue.empty():
+        item = queue.lease(lease_secs=10, block=True, timeout=2)
 
-            try:
-                image = cv2.imread(path)
-            except:
-                raise ValueError('cv2 err')
-            auger = KaleidoscopeAugmenter(image, key["Key"], 6, save=True)
+        if item is not None:
+            batch = pickle.loads(item)
 
-        queue.complete(item)
-        print("Batch processing time : {}".format(time.time() - start))
-    else:
-        print("Waiting for work")
+            for i, key in enumerate(batch):
+
+                image_pointer = key["Key"]
+                download_path = constants.DOWNLOAD_DIRECTORY + image_pointer
+                os.makedirs(os.path.dirname(download_path), exist_ok=True)
+                origin_bucket.download_file(image_pointer, download_path)
+
+                try:
+                    image = cv2.imread(download_path)
+                except:
+                    raise ValueError('cv2 err')
+
+                # TODO: make the number of transformations settable in Interface()
+                auger = KaleidoscopeAugmenter(image, image_pointer, 6)
+                # TODO: Add in calls to auger.transform, auger.save
+            queue.complete(item)
+
+        else:
+            print("Waiting for work")
+
+
+worker(queue)
